fb4b2af0d08de1b0fd74d8137da02e1c854f34f5
Alexander <alexander_batista@bhd.com.do>
2023-03-07 10:25:14 -0400

    first commit

---
+++ src/hooks/useCalendarStore.js
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from "react-redux"
+import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store";
+
+
+export const useCalendarStore = () => {
+
+   const dispatch = useDispatch();
+   const {events,activeEvent} = useSelector(state => state.calendar);
+
+    const setActiveEvent = ( calendarEvent ) => {
+      dispatch(onSetActiveEvent(calendarEvent));
+    }  
+    
+    const startSavingEvent = async( calendarEvent ) => {
+      if(calendarEvent._id){
+        //updating
+        dispatch(onUpdateEvent({...calendarEvent}));
+      }else{
+        //creating
+        dispatch(onAddNewEvent({...calendarEvent, _id: new Date().getTime()}));
+      }
+    }
+
+    const startDeletingEvent = () => {
+      dispatch(onDeleteEvent());
+    }
+
+
+
+  return {
+    //props
+    activeEvent,
+    events,
+    hasEventSelected: !!activeEvent,
+    //methods
+    startDeletingEvent,
+    setActiveEvent,
+    startSavingEvent
+  }
+}
